474ba7f4fc0d1d5b733e38f01196ae05030abb2f
Sven <sven.hecht@gmail.com>
2018-04-20 13:47:20 +0200

    wire to mpu9250

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <NeoMaple.h>
 #include "vmath.h"
+#include "sensor.h"
 
 int ESPI2CAddress = 0x53;
 
@@ -14,8 +15,7 @@ int dir = 0;
 int focus = 20;
 
 int delayval = 100;
-float dots[NUMPIXELS][3];
-float normal[3];
+double previousRotation[3];
 
 void setup()
 {
@@ -23,44 +23,69 @@ void setup()
     pixels.begin();
 
     delay(100);
+    Wire.begin();
 
-    for (int i = 0; i < NUMPIXELS; i++)
-    {
-        dots[i][0] = sin(i / NUMPIXELS * PI * 2);
-        dots[i][1] = cos(i / NUMPIXELS * PI * 2),
-        dots[i][2] = 0;
-    }
+    initSensor();
+
+    Serial.println("Setup done");
 }
 
-float calculateDistance(float dot[3])
+double calculateDistance(int dotIndex, double normal[3])
 {
-    float offset[3] = { 0, 0, .5F };
-    return dotProduct(normal, add(dot, offset));
+    double offset[3] = {0, 0, .5F};
+    double dot[3] = {
+        sin((double)dotIndex / (double)NUMPIXELS * PI * 2.0),
+        cos((double)dotIndex / (double)NUMPIXELS * PI * 2.0),
+        0};
+
+    Serial.print("DOT");
+    Serial.print(dotIndex);
+    Serial.print(" X= ");
+    Serial.print(dot[0]);
+    Serial.print(" Y= ");
+    Serial.print(dot[1]);
+    Serial.print(" Z= ");
+    Serial.println(dot[2]);
+
+    return normal[0] * (dot[0] + offset[0]) +
+           normal[1] * (dot[1] + offset[1]) +
+           normal[2] * (dot[2] + offset[2]);
 }
 
-float calculateIntensity(float distance)
+double calculateIntensity(double distance)
 {
     return floor((1 - distance) * 255);
 }
 
-void updateDot(int dotIndex, float intensity)
+void updateDot(int dotIndex, double intensity)
 {
     pixels.setPixelColor(dotIndex, pixels.Color(0, intensity, 0));
 }
 
-void updateDots()
+void updateDots(double normal[3])
 {
     for (int i = 0; i < NUMPIXELS; i++)
     {
-        float distance = calculateDistance(dots[i]);
-        float intensity = calculateIntensity(distance);
+        double distance = calculateDistance(i, normal);
+        double intensity = calculateIntensity(distance);
         updateDot(i, intensity);
     }
 }
 
+void eulerToVector(float rotation[3], double normal[3])
+{
+    double roll = rotation[0] * PI / 180;
+    double yaw = rotation[1] * PI / 180;
+    double pitch = rotation[2] * PI / 180;
+
+    normal[0] = -sin(yaw) * sin(pitch) * cos(roll) - cos(yaw) * sin(roll);
+    normal[1] = -cos(yaw) * sin(pitch) * cos(roll) + sin(yaw) * sin(roll);
+    normal[2] = cos(pitch) * cos(roll);
+}
+
 void loop()
 {
-    Wire.requestFrom(ESPI2CAddress, 3);
+    /*Wire.requestFrom(ESPI2CAddress, 3);
 
     int X, Y, Z;
     if (Wire.available() <= 3)
@@ -75,11 +100,33 @@ void loop()
     Serial.print(" Y= ");
     Serial.print(Y);
     Serial.print(" Z= ");
-    Serial.println(Z);
+    Serial.println(Z);*/
 
-    updateDots();
+    float rotation[3];
+    double normal[3];
+    readSensor(rotation);
 
-    pixels.show();
+    if (rotation[0] != previousRotation[0]
+        && rotation[1] != previousRotation[1]
+        && rotation[2] != previousRotation[2])
+    {
+        previousRotation[0] = rotation[0];
+        previousRotation[1] = rotation[1];
+        previousRotation[2] = rotation[2];
+
+        eulerToVector(rotation, normal);
+
+        Serial.print("X= ");
+        Serial.print(normal[0]);
+        Serial.print(" Y= ");
+        Serial.print(normal[1]);
+        Serial.print(" Z= ");
+        Serial.println(normal[2]);
+
+        updateDots(normal);
+
+        pixels.show();
+    }
 
     delay(delayval);
 }
--- src/vmath.h
+++ src/vmath.h
@@ -1,8 +1,6 @@
 #ifndef VMATH
 #define VMATH
 
-#include <Arduino.h>
-
 float length(float v[3])
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -15,37 +13,25 @@ float dotProduct(float v1[3], float v2[3])
            v1[2] * v2[2];
 }
 
-float* multiply(float v[3], float scalar)
+void multiply(float v[3], float scalar)
 {
-    float result[3] = {
-        v[0] * scalar,
-        v[1] * scalar,
-        v[2] * scalar
-    };
-
-    return result;
+    v[0] *= scalar;
+    v[1] *= scalar;
+    v[2] *= scalar;
 }
 
-float* add(float v1[3], float v2[3])
+void add(float v1[3], float v2[3])
 {
-    float result[3] = {
-        v1[0] + v2[0],
-        v1[1] + v2[1],
-        v1[2] + v2[2]
-    };
-
-    return result;
+    v1[0] += v2[0];
+    v1[1] += v2[1];
+    v1[2] += v2[2];
 }
 
-float* divide(float v[3], float scalar)
+void divide(float v[3], float scalar)
 {
-    float result[3] = {
-        v[0] / scalar,
-        v[1] / scalar,
-        v[2] / scalar
-    };
-
-    return result;
+    v[0] /= scalar;
+    v[1] /= scalar;
+    v[2] /= scalar;
 }
 
 #endif
